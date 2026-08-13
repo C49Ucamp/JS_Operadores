38d7ad13f5d82b93e9b43009d5b932d9de16bee3
Dany PM <danniscreamer@gmail.com>
2026-08-12 19:56:14 -0600

    Se añadió los ejercicios vistos relacionados al DOM

--- index.js
+++ index.js
@@ -1,54 +1,38 @@
-class Rectangulo {
-  constructor(alto,ancho){
-    this.alto = alto;
-    this.ancho = ancho; 
-  }
+console.log("Hola mundo"); 
 
-  get rectangulo(){
-     return this.calculoArea();
-  }
+function pesos(){
+  let pesos = document.getElementById("pesos").value
+  let dolar = 17
+  let res = document.getElementById("resultado")
 
-  calculoArea(){
-    return this.alto * this.ancho; 
-  }
+  res.innerHTML = "Dolares: " + (pesos / dolar)
 }
 
-const rec = new Rectangulo(20,10); 
 
-console.log(rec.rectangulo); 
+document.addEventListener('DOMContentLoaded', function(){
+  let nombre = document.getElementById("nombre")
+  let mensaje = document.getElementById("mensaje")
 
-
-class Animal {
-  constructor(nombre){
-    this.nombre = nombre;
+  nombre.onchange = function(){
+    mensaje.textContent = "Mensaje: Tu nombre es " + nombre.value;
   }
+} )
 
-  get Nombre(){
-    return this.llamar(); 
+let y = Math.floor(Math.random() * 10 + 1);
+let adivinar = 1; 
+
+function adivina(){
+  let x = document.getElementById("adivina").value;
+  if(x == y){
+    console.log("acertaste"+ x + "=" + y)
+    document.getElementById("result").innerHTML = "¡Acertaste!"
+  } else if (x > y){
+    adivinar++;
+    console.log("Intenta con un número más pequeño"+ x + "=" + y);
+    document.getElementById("result").innerHTML = "Intenta un número más pequeño"
+  } else {
+    adivinar++;
+    console.log("Intenta un número más grande"+ x + "=" + y)
+    document.getElementById("result").innerHTML = "Intenta un número más grande"
   }
-
-  llamar(){
-    console.log("Estoy llamando a:" + this.nombre);
-  }
 }
-
-class Perro extends Animal {
-constructor(nombre, raza){
-super(nombre)
-this.raza = raza; 
-}
-
-ladra(){
- super.llamar();
- console.log(`${this.raza} es muy bonito`)
-}
-}
-
-const miPerro = new Perro("Rex", "Pastor Alemán");
-miPerro.ladra(); 
-
-
-
-// const miPerro = new Animal("Firulais")
-// console.log(miPerro.Firulais); 
-
